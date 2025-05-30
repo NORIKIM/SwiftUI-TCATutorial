0fe470ce64d321eac7403d1d76575b743527ea8e
NORIKIM <jinagold.k@gmail.com>
2025-05-30 09:25:59 +0900

    Add/프로젝트 생성

---
+++ SwiftUITCATutorial/SwiftUITCATutorial/SwiftUITCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITCATutorialApp.swift
+//  SwiftUITCATutorial
+//
+//  Created by iOS Dev on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
