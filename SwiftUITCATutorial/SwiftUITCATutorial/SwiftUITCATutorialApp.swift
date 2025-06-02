5e8bfff785e19c4a04a99d133c82f4316cbff7d5
NORIKIM <jinagold.k@gmail.com>
2025-06-02 09:25:03 +0900

    Chap1/Your first feature/Integrating into the app

--- SwiftUITCATutorial/SwiftUITCATutorial/SwiftUITCATutorialApp.swift
+++ SwiftUITCATutorial/SwiftUITCATutorial/SwiftUITCATutorialApp.swift
@@ -5,13 +5,19 @@
 //  Created by iOS Dev on 5/30/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct SwiftUITCATutorialApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: SwiftUITCATutorialApp.store)
         }
     }
 }
