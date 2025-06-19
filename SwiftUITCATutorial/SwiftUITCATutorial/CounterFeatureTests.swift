1b4a4c6f352c8e0b9dda74183bc270777afa3529
NORIKIM <jinagold.k@gmail.com>
2025-06-19 20:01:44 +0900

    Chap3/Testing your featrure/Testing effects

--- SwiftUITCATutorial/SwiftUITCATutorial/CounterFeatureTests.swift
+++ SwiftUITCATutorial/SwiftUITCATutorial/CounterFeatureTests.swift
@@ -13,16 +13,23 @@ import Testing
 @MainActor
 struct CounterFeatureTests {
     @Test
-    func basics() async {
+    func timer() async {
+        let clock = TestClock()
         let store = TestStore(initialState: CounterFeature.State()) {
             CounterFeature()
+        } withDependencies: {
+            $0.continuousClock = clock
         }
         
-        await store.send(.incrementButtonTapped) {
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+        }
+        await clock.advance(by: .seconds(1))
+        await store.receive(\.timerTick) {
             $0.count = 1
         }
-        await store.send(.decrementButtonTapped) {
-            $0.count = 0
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = false
         }
     }
 }
