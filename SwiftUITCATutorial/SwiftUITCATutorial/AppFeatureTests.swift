8ae0add6451c8017a59da04e6c775c5e2bf1d4b7
NORIKIM <jinagold.k@gmail.com>
2025-06-24 15:44:19 +0900

    Chap4/Composing features/Composing reducers

---
+++ SwiftUITCATutorial/SwiftUITCATutorial/AppFeatureTests.swift
@@ -0,0 +1,25 @@
+//
+//  AppFeatureTests.swift
+//  SwiftUITCATutorial
+//
+//  Created by iOS Dev on 6/24/25.
+//
+
+import ComposableArchitecture
+import Testing
+
+@testable import SwiftUITCATutorial
+
+@MainActor
+struct AppFeatureTests {
+    @Test
+    func incrementInFirstTab() async {
+        let store = TestStore(initialState: AppFeature.State()) {
+            AppFeature()
+        }
+        
+        await store.send(\.tab1.incrementButtonTapped) {
+            $0.tab1.count = 1
+        }
+    }
+}
